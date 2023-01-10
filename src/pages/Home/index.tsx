a907e68849cf9638e0f736167139a72c4e9eecdb
Eris Seris <tsremarkable@gmail.com>
2023-01-11 07:42:42 +0900

    add: render interface<array> using map

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { useState } from "react";
+import { userFormData, UserFormItem } from "./userform";
 const Home = () => {
     const [count, setCount] = useState(0);
-    
+    const [myinfo, setUserForm] = useState<UserFormItem>({
+        id: 3,
+        label: 'Soda'
+    });
     const handleIncrease = () => {
         setCount(count + 1);
     };
+    const items = userFormData.map(item => (
+        <div key={item.id}>
+            <p>{item.id}</p>
+            <p>{item.label}</p>
+        </div>
+    ))
     return (
         <>
             <h1 className="text-3xl font-bold underline">This is HomePage</h1>
@@ -14,6 +24,11 @@ const Home = () => {
                     Increase
                 </button>
                 <div>{ count }</div>
+                {items}
+                <div>
+                    <p>{myinfo.id} : {myinfo.label}</p>
+                    <p></p>
+                </div>
             </div>
         </>
     );
---
+++ src/pages/Home/userform.ts
@@ -0,0 +1,11 @@
+export interface UserFormItem {
+  id: number;
+  label: string;
+}
+
+
+export const userFormData: UserFormItem[] = [
+  { "id": 0, "label": 'Starbuck'},
+  { "id": 1, "label": 'Vodka'},
+  { "id": 2, "label": 'Champagne'}
+]
