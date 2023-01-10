cb185ec47e062a580eaf8b74f1243b48e123f03d
Eris Seris <tsremarkable@gmail.com>
2023-01-11 06:59:08 +0900

    func: useState function

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,20 @@
 import React from "react";
-
+import { useState } from "react";
 const Home = () => {
+    const [count, setCount] = useState(0);
+    
+    const handleIncrease = () => {
+        setCount(count + 1);
+    };
     return (
         <>
             <h1>This is HomePage</h1>
+            <div>
+                <button type="button" onClick={handleIncrease}>
+                    Increase
+                </button>
+                <div>{ count }</div>
+            </div>
         </>
     );
 }
