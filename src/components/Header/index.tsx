0fd753006b37af4dd227a62101c332fc1ce4c49a
Eris <tsremarkable@gmail.com>
2023-01-27 13:54:53 -0500

    add: useContext part

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import './Header.scss'
 const Header = () => {
     return (
         <>
-            <div className="h-10 header-shadow text-zinc-100 text-3xl flex justify-center mb-10 p-10 items-center">
+            <div className="object-top sticky inset-x-0 top-0 left-0 h-10 header-shadow text-zinc-100 text-3xl flex justify-center mb-10 p-10 items-center">
                 <h1>React Hooks Tutorial</h1>
             </div>
         </>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,17 @@
 import React, { EventHandler, MouseEvent, MouseEventHandler } from "react";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, createContext, useContext } from "react";
 import { userFormData, UserFormItem } from "./userform";
 import { useWindowListener } from "./useWindowListener";   // custom useEffect hook
 import { CounterTracker } from "./countertracker";
 import Header from '../../components/Header';
 import './home.scss';
+
+type ThemeContextType = "#565655" | "black";
+const ThemeContext = createContext<ThemeContextType>("#565655");
+
 const Home = () => {
+    const [color, setColor] = useState<ThemeContextType>("#565655");
+    const theme = useContext(ThemeContext);
     const [count, setCount] = useState(0);                      //useState
     const [myinfo, setUserForm] = useState<UserFormItem>({
         id: 3,
@@ -39,6 +45,7 @@ const Home = () => {
     })
     return (
         <>
+           <Header />
             <div style={{
                 position: 'absolute',
                 backgroundColor: 'pink',
@@ -53,7 +60,6 @@ const Home = () => {
             }}>
 
             </div>
-            <Header />
             <div className="flex flex-col justify-center items-center p-5">
                 <div className="flex justify-center items-center mb-10">
                     <button type="button" className="btn mr-5" onClick={handleIncrease}>
@@ -66,13 +72,37 @@ const Home = () => {
                 </div>
                 <div className="flex flex-col justify-center decoration-white text-2xl">
                     <div className="flex justify-center m-8">
-                        <button className="btn mr-5 w-16" onClick={handleIncrement}>+</button>
-                        <button className="btn mr-5 w-16" onClick={handleDecrement}>-</button>
+                        { color === "black" ? (
+                            <>
+                            <button className="btn mr-5 w-16 bg-black" onClick={handleIncrement}>+</button>
+                            <button className="btn mr-5 w-16  bg-black" onClick={handleDecrement}>-</button>
+                            </>  
+                        ) :
+                            <>
+                            <button className="btn mr-5 w-16" onClick={handleIncrement}>+</button>
+                            <button className="btn mr-5 w-16" onClick={handleDecrement}>-</button>
+                            </>
+                        }    
                     </div>
                     <div className="justify-center flex mb-8 col-wheat">Count  : {value}</div>
                     <div className="justify-center flex mb-8 col-wheat">Buttons {ref.current.increment + ref.current.decrement} times clicked</div>
                     <div className="justify-center flex mb-8 col-wheat">Increment clicked: {ref.current.increment}</div>
                     <div className="justify-center flex mb-8 col-wheat">Decrement clicked: {ref.current.decrement}</div>
+                </div>
+                <div>
+                  <ThemeContext.Provider value={color}>
+                        <label>
+                            <input
+                            type="checkbox"
+                            checked={color === 'black'}
+                            onChange={(e) => {
+                                setColor(e.target.checked ? 'black' : '#565655')
+                                console.log(color)
+                            }}
+                            />
+                            Use dark mode
+                        </label>
+                    </ThemeContext.Provider>
                 </div>
             </div>
         </>
